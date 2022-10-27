b5e513809788d72628d692bf88c8c23c03a2742a
para_nc <viktor.ov4arenko@gmail.com>
2022-10-28 01:18:02 +0300

    1.0.0

---
+++ src/main/java/api/smartsender/io/UpdateContactData.java
@@ -0,0 +1,12 @@
+package api.smartsender.io;
+
+public class UpdateContactData {
+
+
+    public static void main( String[] args )
+        {
+            System.out.println( "Hello World!" );
+        }
+
+
+}
